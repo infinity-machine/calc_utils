aa857442e0799c268b71cf771bd914af9d0909bd
Connor Carciofini <ccarciofini@gmail.com>
2024-12-27 11:38:04 -0600

    pairs init

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,26 +7,19 @@
 
 int main()
 {
-    std::vector<double> coeffs = {2, 3, 1, 2};
-    std::vector<double> pwrs = {4, 3, 2, 1};
+    // Polynomial poly3 = poly1 + poly2;
+    // poly1.print();
+    // std::cout << "-----" << std::endl;
+    // poly2.print();
+    // std::cout << "-----" << std::endl;
+    // poly3.print();
 
-    Polynomial poly1(coeffs, pwrs);
-    Polynomial poly2({1, 1, 1, 1});
-    std::cout << poly1.evaluate(2) << std::endl;
-    std::cout << poly2.evaluate(2) << std::endl;
-
-    Polynomial poly3 = poly1 + poly2;
-    poly1.print();
-    std::cout << "-----" << std::endl;
-    poly2.print();
-    std::cout << "-----" << std::endl;
-    poly3.print();
-
-    std::vector<std::pair<int, int>> pairvector;
+    std::vector<std::pair<double, double>> pairvector;
 
     pairvector.push_back({1, 2});
 
     std::cout << pairvector[0].second << std::endl;
 
-
+    Polynomial poly1({1, 3, 4});
+    poly1.print();
 }
